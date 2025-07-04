50ead7af8343fd6ec59270c641dca1ddf4dd47d3
Nicoly1809 <nicoly.paganini.soares@escola.pr.gov.br>
2025-07-04 20:11:32 -0300

    Aula 10

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'História Antiga',
+    'Quais eram as principais características da civilização egípcia?',
+    'Teocracia, politeísmo, escrita hieroglífica, pirâmides e agricultura no Nilo'
+)
+
+criaCartao(
+    'Idade Média',
+    'Oque foi a Peste Negra e qual seu impacto na Europa?',
+    'Epidemia que matou milhões e abalou a estrutura feudal'
+)
+
+criaCartao(
+    'Idade Moderna',
+    'O que foi a Reforma Protestante?',
+    'Movimento religioso que rompeu com a Igreja Católica e originou o protestantismo.'
+)
+criaCartao(
+    'História do Brasil',
+    'O que foi o período regencial?',
+    'Fase de transição entre Dom Pedro I e Dom Pedro II, marcada por instabilidade.'
+)
+
+criaCartao(
+  'História Contemporânea',
+  'Como surgiu o nazismo na Alemanha?',
+  'Após a crise do pós-guerra e a humilhação do Tratado de Versalhes.'
+)
+
+criaCartao(
+    'História Antiga',
+    'Quais as diferenças entre Esparta e Atenas?',
+    'Esparta era militarista e oligárquica; Atenas, cultural e democrática.'
+)
+
+criaCartao(
+    'Idade Média',
+    'Quais eram os três grupos sociais principais no sistema feudal?',
+    'Nobreza, clero e servos.'
+)
+
+criaCartao(
+    'Idade Moderna',
+    'O que motivou a Revolução Francesa?',
+    'Desigualdade social, crise econômica e ideias iluministas.'
+)
+
+criaCartao(
+    'História do Brasil',
+    'O que foi o ciclo do açúcar no Brasil?',
+    'Período em que a produção e exportação de açúcar dominavam a economia colonial.'
+)
+
+criaCartao(
+    'História Contemporânea',
+    'O que foi a Guerra Fria?',
+    'Disputa ideológica, militar e política entre EUA (capitalismo) e URSS (socialismo).'
+)
+
+criaCartao(
+    'História do Cinema',
+    'Quem foram os Irmãos Lumière e qual sua contribuição para o cinema?',
+    'Inventores franceses que criaram o cinematógrafo e exibiram os primeiros filmes no final do século'
+)
+
+criaCartao(
+    'História da Filosofia',
+    'Quem foi Sócrates e qual sua contribuição para a filosofia?',
+    'Filósofo grego que ensinava por meio do diálogo e defendia o autoconhecimento como chave da sabedoria.'
+)
