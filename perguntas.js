27e6b0ae6d38b773ea7fcd9d361abb6d0d4ecf6c
Nicoly1809 <nicoly.paganini.soares@escola.pr.gov.br>
2025-07-04 19:40:46 -0300

    Aula 7

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'História Antiga',
+    'Quais eram as principais características da civilização egípcia?',
+    'Teocracia, politeísmo, escrita hieroglífica, pirâmides e agricultura no Nilo'
+)
+
+criaCartao(
+    'Idade Média',
+    'Oque foi a Peste Negra e qual seu impacto na Europa?',
+    'Epidemia que matou milhões e abalou a estrutura feudal'
+)
+
+criaCartao(
+    'Idade Moderna',
+    'O que foi a Reforma Protestante?',
+    'Movimento religioso que rompeu com a Igreja Católica e originou o protestantismo.'
+)
